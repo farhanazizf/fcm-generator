997c957710d7aaad5ffa16b6cfedd3a06a01e7f1
farhanazizf-wartek <farhan.aziz@wartek.belajar.id>
2024-09-29 19:23:29 +0700

    add github pages for deploy

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+// Import Firebase scripts required for the service worker
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js"
+);
+
+// Initialize the Firebase app in the service worker with your Firebase configuration
+firebase.initializeApp({
+  apiKey: "AIzaSyAs2luvoFGmNIn0CvuT4oHmww-bQpo1kgo",
+  authDomain: "rabbitmq-notif.firebaseapp.com",
+  projectId: "rabbitmq-notif",
+  storageBucket: "rabbitmq-notif.appspot.com",
+  messagingSenderId: "486447015747",
+  appId: "1:486447015747:web:4b9159652b927cee1ef1f6",
+});
+
+// Retrieve an instance of Firebase Messaging to handle background messages
+const messaging = firebase.messaging();
+
+// Handle background messages
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+
+  // Customize the notification
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/logo192.png", // Adjust this path to point to your notification icon
+  };
+
+  // Display the notification
+  // eslint-disable-next-line no-restricted-globals
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log("x");
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register(`${process.env.PUBLIC_URL}/firebase-messaging-sw.js`)
-    .then((registration) => {
-      console.log("Service Worker registered with scope:", registration.scope);
-    })
-    .catch((error) => {
-      console.error("Service Worker registration failed:", error);
-    });
-}
+// console.log("x");
+// // if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register(`/firebase-messaging-sw.js`)
+//     .then((registration) => {
+//       console.log("Service Worker registered with scope:", registration.scope);
+//     })
+//     .catch((error) => {
+//       console.error("Service Worker registration failed:", error);
+//     });
+// }
 
 root.render(
   <React.StrictMode>
